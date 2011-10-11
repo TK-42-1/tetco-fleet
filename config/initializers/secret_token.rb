5c2f3720515ab63c3b8d76be311a372036a2ddcf
TK-42-1 <mister.teekay@gmail.com>
2011-10-11 14:44:53 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TetcoFleet::Application.config.secret_token = '5d97d3b0efa93d8159c0a1daececcb6f4088eef4b404c3e0d75394218c273e6f1fdd84a7009be56b8068e53bc0122b62ec33590902bf5280f91404c48786b6c6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TetcoFleet::Application.config.session_store :cookie_store, key: '_tetco_fleet_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TetcoFleet::Application.config.session_store :active_record_store
